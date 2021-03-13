2a3fe2056ad1f7bc25e794b8ca4a4bdd341c5d4e
Megh Parikh <meghprkh@gmail.com>
2021-03-13 18:53:59 +0000

    Add pump adding logic

--- grid.gd
+++ grid.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-
-func _ready():
-	pass  # Replace with function body.
-
+onready var pump_scene = preload("res://objects/pump.tscn")
 
 func _input(event):
 	# Mouse in viewport coordinates.
@@ -13,4 +10,10 @@ func _input(event):
 	clicked_at = clicked_at.floor()
 	if clicked_at.x > C.GRID_COLS or clicked_at.y > C.GRID_ROWS:
 		return
-	print(clicked_at)
+	add_pump(clicked_at)
+	
+func add_pump(position):
+	var pump = pump_scene.instance()
+	pump.global_position = position * C.CELL_SIZE + self.global_position
+	$blocks.add_child(pump)
+	print("Added pump at ", position)
